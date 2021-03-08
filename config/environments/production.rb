3423cde6608dbc8e374c3da812671e577abb6eab
IlyaKokorev <kockorev.ilya@gmail.com>
2021-03-08 17:29:01 +0300

    init to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,10 +41,10 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://instatalk.herokuapp.coom/cable'
+  config.action_cable.url = 'wss://ricktalk.herokuapp.coom/cable'
   config.action_cable.allowed_request_origins = [
-    'http://instatalk.herokuapp.coom/cable',
-    'https://instatalk.herokuapp.coom/cable'
+    'http://ricktalk.herokuapp.coom/cable',
+    'https://ricktalk.herokuapp.coom/cable'
   ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
